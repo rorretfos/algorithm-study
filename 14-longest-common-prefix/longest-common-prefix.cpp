5090326d7bba2c01281e2b3922c44d0f2b1acdcf
Charlie Kim <gnuoynismik@gmail.com>
2026-06-30 00:30:51 -0400

    Time: 0 ms (100.00%) | Memory: 11.8 MB (58.39%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string prefix = "";
         string First = strs[0];
-        int add = First.size();
+
         for(int i = 0; i < strs.size(); i++)
         { 
             int j = 0;
@@ -12,13 +11,40 @@ public:
             {
                 j++;
             }
-            add = min(add, j);
+            First = First.substr(0,j);
+
+            
+            if (First.empty())
+                return "";
         }
-        for (int k = 0; k < add; k++)
-        {
-            prefix += First[k];
-        }
-        return prefix;
-        
+        return First;
     }
 };
+
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         string First = strs[0];
+
+//         for (int i = 1; i < strs.size(); i++)
+//         {
+//             int j = 0;
+
+//             while (j < First.size() &&
+//                    j < strs[i].size() &&
+//                    First[j] == strs[i][j])
+//             {
+//                 j++;
+//             }
+
+//             // 공통 접두사만 남긴다.
+//             First = First.substr(0, j);
+
+//             // 더 이상 공통 접두사가 없으면 끝
+//             if (First.empty())
+//                 return "";
+//         }
+
+//         return First;
+//     }
+// };
