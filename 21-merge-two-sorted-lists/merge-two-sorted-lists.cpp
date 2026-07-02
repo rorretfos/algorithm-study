e2ce00548e9edf5ec7e809667cd33ec0b56b9b3a
Charlie Kim <gnuoynismik@gmail.com>
2026-07-02 15:36:04 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (62.41%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,32 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-        // 결과 리스트의 시작을 위한 더미 노드
         ListNode dummy(0);
-
-        // 항상 결과 리스트의 마지막 노드를 가리킴
         ListNode* tail = &dummy;
 
-        while (list1 != nullptr && list2 != nullptr) {
-
-            if (list1->val <= list2->val) {
+        while (list1 != nullptr && list2 != nullptr)
+        {
+            if(list1->val <= list2->val)
+            {
                 tail->next = list1;
                 list1 = list1->next;
             }
-            else {
+            else
+            {
                 tail->next = list2;
-                list2 = list2->next;
+                list2 = list2 -> next;
             }
-
-            tail = tail->next;
+            tail = tail -> next;
         }
-
-        // 남은 리스트를 그대로 연결
+        
         if (list1 != nullptr)
-            tail->next = list1;
+            tail -> next = list1;
         else
-            tail->next = list2;
+            tail -> next = list2;
 
         return dummy.next;
     }
